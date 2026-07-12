fbe40ee01ac2dfac121d54703fd48e5e2f267486
hijker <jacktomy107@gmail.com>
2026-07-12 12:44:17 +0530

    Moved to groq since no other provider gives free usage :(

--- chatapp/chat_service.py
+++ chatapp/chat_service.py
@@ -5,11 +5,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv("OPENAI_KEY"))
+client = OpenAI(api_key=os.getenv("GROQ_API_KEY"),
+                base_url="https://api.groq.com/openai/v1")
 
 class ChatServ:
     def get_response(self, input : str) -> str:
-        return client.responses.create(
-            model="gpt-4o-mini",
+        response = client.responses.create(
+            model="llama-3.3-70b-versatile",
             input=input
         )
+        print (response.model_dump_json(indent=2))
+        return response.output[1].content[0].text
