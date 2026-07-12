6353c2107fb9747827a5580c1b34ba5bf8af557a
hijker <jacktomy107@gmail.com>
2026-07-12 13:47:56 +0530

    Add memory

--- chatapp/chat_service.py
+++ chatapp/chat_service.py
@@ -9,10 +9,30 @@ client = OpenAI(api_key=os.getenv("GROQ_API_KEY"),
                 base_url="https://api.groq.com/openai/v1")
 
 class ChatServ:
+    
+    
+    def __init__(self):
+        self.messages = [
+            {
+                "role": "system",
+                "content": "You are a mathematician"
+            }
+        ]
+    
+    
     def get_response(self, input : str) -> str:
+        
+        self.messages.append({
+            "role":"user",
+            "content" : input
+        })
         response = client.responses.create(
             model="llama-3.3-70b-versatile",
-            input=input
+            input=self.messages
         )
-        print (response.model_dump_json(indent=2))
-        return response.output[1].content[0].text
+        # print (response.model_dump_json(indent=2))
+        self.messages.append({
+            "role":"assistant",
+            "content" : response.output_text
+        })
+        return response.output_text
